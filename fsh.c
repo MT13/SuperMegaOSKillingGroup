ee81ca91ac89deefbb5a1c4536a8f0aa855f8a6d
givil3o <givil3o5@gmail.com>
2018-03-04 17:05:33 +0400

    added multi command execution per line(;)/ pwd/exit now succeeds on tests

--- fsh.c
+++ fsh.c
@@ -6,12 +6,14 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+char* substring(char* input,int len){
+	char* buffer=(char*)malloc(sizeof(char*)*(len+1));
+	memcpy(buffer,input,len);
+	buffer[len]='\0';
+	return buffer;
+}
 
-int main(){
-	char* input;
-	while(1){
-		input=readline("Free Shell: ");
-
+void execute(char* input){
 		if(strncmp(input,"pwd",3)==0){
 			char* a=getcwd(0,0);
 			printf("%s\n",a);
@@ -21,11 +23,38 @@ int main(){
 			int id=chdir(input+3);
 			if(id==-1){}// such directory does not exist
 		}
-		if(strncmp(input,"exit",4)==0){
-			if(strlen(input)==4)return 0;
-			else return input[5]-'0';
+		if(strlen(input)>=4&&strncmp(input,"exit",4)==0){
+			int exitCode=0;
+			if(strlen(input)>4)exitCode=input[5]-'0';
+			free(input);
+			exit(exitCode);
 		}
 		free(input);
+}
+
+
+int main(int argc,char** argv){
+	char* input;
+	int n,previousPoint;
+
+	while(1){
+		previousPoint=0;
+		if(argc==3)input=argv[2];
+		else input=readline("free shell: ");
+
+		n=strlen(input);
+
+		for(int i=0;i<n;i++){
+			if(input[i]==';'||i==n-1){
+				if(i==n-1)i++;
+				execute(substring(input+previousPoint,i-previousPoint));
+				previousPoint=i+1;
+			}
+		}
+		
+
+		if(argc!=3)free(input);
+		else break;
 	}
 	return 0;
 }
