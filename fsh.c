82e18829f98b28779ee47e21c40172f5077a0d2c
giorgi <gmarg10@freeuni.edu.ge>
2018-03-05 18:07:59 +0400

    echo commant implemented
    echo commant implemented; wotks with variables too ($PATH); but commant "$?" yet to implement

--- fsh.c
+++ fsh.c
@@ -49,6 +49,45 @@ void  signalKiller(int s){
 	}
 }
 
+void inputEcho(char * input){
+	char* inputCopy=(char*)malloc(sizeof(char*)*(strlen(input) - 4));
+	memcpy(inputCopy,input + 4,strlen(input) - 4);
+	inputCopy[strlen(input) - 4]='\0';
+
+	int found = -1;
+
+	if (strlen(input) > 4){
+		while (*inputCopy++){
+			if (strncmp(&inputCopy[0],"$",1)==0){
+				found = 1;
+			} else if (strncmp(&inputCopy[0]," ",1) !=0){
+				break;
+			}
+		}
+	}
+
+	if (found == 1){
+		strtok (inputCopy," ");
+
+		if (inputCopy != NULL){
+			if (strncmp(inputCopy,"?", 1) == 0){
+				// bolo shvilobilis status code unda ak
+			} else {
+				char * result = getenv(inputCopy);
+				if (result != NULL){
+					printf("%s\n", result);
+				} else {
+					printf("\n");
+				}
+			}
+		} else {
+			printf("%s\n", input+4);
+		}
+	} else {
+		printf("%s\n", inputCopy);
+	}
+}
+
 void execute(char* input){
 		int n=strlen(input);
 		if(strncmp(input+n-3,INPUT_PWD,3)==0){
@@ -80,6 +119,12 @@ void execute(char* input){
 		if(strncmp(input,INPUT_NICE,4)==0){
 			if(strlen(input)==4)printf("%d\n",nice(0));
 			else printf("%d\n",nice(stringToInt(input,8,strlen(input))));
+		}
+
+		if(strlen(input) >=4 && strncmp(input, INPUT_ECHO, 4) == 0){
+			if (strlen(input) == 4 || (strlen(input) > 4 && strncmp(&input[4]," ",1) == 0)){
+				inputEcho(input);
+			}
 		}
 
 		free(input);
